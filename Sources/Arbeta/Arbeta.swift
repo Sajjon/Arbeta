447d44b8cc304ecedd933400e443f516c9a794d3
Alexander Cyon <alex.cyon@gmail.com>
2022-02-08 15:38:13 +0100

    swift package init

---
+++ Sources/Arbeta/Arbeta.swift
@@ -0,0 +1,6 @@
+public struct Arbeta {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/ArbetaTests/ArbetaTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Arbeta
+
+final class ArbetaTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Arbeta().text, "Hello, World!")
+    }
+}
